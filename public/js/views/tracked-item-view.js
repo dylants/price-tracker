994ad27ad937a626778ace0e4ec129f3737e8cba
Dylan Smith <dylantsmith@gmail.com>
2014-01-11 23:27:38 -0600

    Note price changes in the past 24 hours
    Highlight tracked items that have prices which have recently been
    updated (within the past 24 hours) in the UI.

--- public/js/views/tracked-item-view.js
+++ public/js/views/tracked-item-view.js
@@ -24,7 +24,7 @@ define([
         },
 
         render: function() {
-            var model;
+            var model, currentPriceDate, yesterday;
 
             // because we're using accounting, and it's an unnamed module,
             // we must specifically pass it to the template on rendering so
@@ -33,6 +33,22 @@ define([
             model.accounting = accounting;
 
             this.$el.html(this.template(model));
+
+            // if the item's current price was recently found, note that in the UI
+            // (only do this if there are past prices)
+            if (model.pastPrices[0]) {
+                currentPriceDate = moment(model.currentPriceDate);
+                yesterday = moment(new Date());
+                yesterday.subtract("days", 1);
+                if (currentPriceDate.isAfter(yesterday)) {
+                    if (model.currentPrice < model.pastPrices[0]) {
+                        $(this.el).find(".tracked-item").addClass("recent-price-decrease");
+                    } else {
+                        $(this.el).find(".tracked-item").addClass("recent-price-increase");
+                    }
+                }
+            }
+
             return this;
         },
 
