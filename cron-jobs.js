f475f661be6123c4699c0b0ac026abb1d661a8c8
Dylan Smith <dylantsmith@gmail.com>
2014-01-12 23:20:28 -0600

    Update the cron job to run every 6 hours
    At least in theory this should work...

--- cron-jobs.js
+++ cron-jobs.js
@@ -1,8 +1,8 @@
 var cronJob = require("cron").CronJob,
     priceUpdater = require("./lib/price-updater");
 
-// Run this cron job every day at 3am
-new cronJob("00 00 03 * * *", function() {
+// Run this cron job every 6 hours
+new cronJob("00 00 */6 * * *", function() {
     console.log("Cron job: triggering a price update");
     priceUpdater.updateTrackedItems();
 }, null, true);
