76069eb666b161c81eb201caee005c2d66410b1b
Dylan Smith <dylantsmith@gmail.com>
2014-01-19 22:08:41 -0600

    Attempt at a phantom version of flights
    Instead of using zombie, use phantom to possibly execute the same code
    path as zombie did to get the flight costs.  However, there's
    definitely some learning I need to do, or find another solution, since
    the phantom path doesn't seem to wait after events are triggered
    (clicks that change pages), so I'm forced to add sleeps.  Because of
    this, it doesn't seem like the solution that should be chosen.  Maybe
    there's a better one out there?

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,6 @@
 var Browser = require("zombie"),
-    moment = require("moment");
+    moment = require("moment"),
+    phantom = require("phantom");
 
 module.exports = function(app) {
     app.namespace("/api", function() {
@@ -30,6 +31,42 @@ module.exports = function(app) {
                         result.outboundPrices = browser.text(".outboundCalendar table");
                         result.inboundPrices = browser.text(".returnCalendar table");
                         res.send(result);
+                    });
+                });
+            });
+        });
+
+        app.get("flights-phantom", function(req, res) {
+            phantom.create(function(ph) {
+                ph.createPage(function(page) {
+                    page.open("http://www.southwest.com", function(status) {
+                        var today;
+
+                        today = moment(new Date());
+                        page.evaluate(function(today) {
+                            $("#originAirport").val("AUS");
+                            $("#destinationAirport").val("SJC");
+
+                            $("#outboundDateAir").val(today);
+                            $("#returnDateAir").val(today);
+
+                            $("#booking_widget_content_row_btn_search").click();
+                        }, function() {
+                            setTimeout(function() {
+                                page.evaluate(function() {
+                                    $(".shortcutNotification a")[0].click();
+                                }, function() {
+                                    setTimeout(function() {
+                                        page.evaluate(function() {
+                                            return document.title;
+                                        }, function(title) {
+                                            res.send("title is!: " + title);
+                                            ph.exit();
+                                        });
+                                    }, 3000);
+                                });
+                            }, 2000);
+                        }, today.format("MM/DD/YYYY"));
                     });
                 });
             });
