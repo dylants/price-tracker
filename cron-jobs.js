9e25b848b2471353a69b7bed40158fc5fd54a051
Dylan Smith <dylantsmith@gmail.com>
2014-02-08 20:47:42 -0600

    Look less robotic
    Sleep random amounts of time between server requests to gather price
    details in an attempt to look less robotic.  Who knows if this will
    actually do anything...

--- cron-jobs.js
+++ cron-jobs.js
@@ -1,8 +1,17 @@
 var cronJob = require("cron").CronJob,
     priceUpdater = require("./lib/price-updater");
 
-// Run this cron job every 6 hours
-new cronJob("00 00 */6 * * *", function() {
-    console.log("Cron job: triggering a price update");
-    priceUpdater.updateTrackedItems();
+// Run this cron job every Sunday (0) at 7:00:00 AM
+new cronJob("00 00 7 * * 0", function() {
+    console.log("Cron job: triggering a price update at " + new Date());
+
+    // before issuing a request to update the tracked items, sleep
+    // anywhere between 1 minute and 180 minutes (3 hours) in an
+    // attempt (which maybe futile) to make ourselves look less
+    // robotic to websites
+    var sleepTime = utils.generateNum(60000, 10800000);
+    console.log("sleeping for " + sleepTime + " milliseconds");
+    setTimeout(function() {
+        priceUpdater.updateTrackedItems();
+    }, sleepTime);
 }, null, true);
